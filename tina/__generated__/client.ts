dfe344ebf53e2fed0c9836f2b63276c8bca63a73
Kaio <kaiobarb@gmail.com>
2025-12-02 21:46:01 -0800

    .

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/kaio/Dev/liftaris.dev/tina/__generated__/.cache/1764740124205', url: 'http://localhost:4001/graphql', token: '232cac03e9cdcdff783c1b871abdf5c1b1d7effa', queries,  });
+export const client = createClient({ cacheDir: '/home/kaio/Dev/liftaris.dev/tina/__generated__/.cache/1764740729044', url: 'https://content.tinajs.io/2.0/content/260ec15c-da69-4817-9885-ad30c27199be/github/main', token: '232cac03e9cdcdff783c1b871abdf5c1b1d7effa', queries,  });
 export default client;
   
