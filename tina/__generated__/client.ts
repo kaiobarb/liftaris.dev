55b99e822ba6e2a9c4e4e1600d9cea2293537781
Kaio <kaiobarb@gmail.com>
2025-12-02 21:35:52 -0800

    Add tina-lock.json and update generated schema

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '232cac03e9cdcdff783c1b871abdf5c1b1d7effa', queries,  });
+export const client = createClient({ cacheDir: '/home/kaio/Dev/liftaris.dev/tina/__generated__/.cache/1764740124205', url: 'http://localhost:4001/graphql', token: '232cac03e9cdcdff783c1b871abdf5c1b1d7effa', queries,  });
 export default client;
   
